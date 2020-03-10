235d0d1492a4562e342b086702d38519c3f86ba3
sklampt <sklampt@student.ethz.ch>
2020-03-10 14:42:16 +0100

    some minor changes

--- task1/task1b/main.py
+++ task1/task1b/main.py
@@ -1,6 +1,6 @@
 ################################################################################
 ############################SINA - FABIEN - ANNA################################
-##########################MACHINE LEARNING TASK1A###############################
+##########################MACHINE LEARNING TASK1B###############################
 ################################################################################
 
 import numpy as np
@@ -8,7 +8,6 @@ from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import Ridge
 
 # Load training data
-#
 # np.set_printoptions(precision=16)#set precision for proper data loading
 train = np.loadtxt(
     'data/train.csv',
@@ -29,7 +28,6 @@ x_train_extended=np.block([x_train,x_train_sq,x_train_exp,x_train_cos,ones])
 
 
 # Train the model
-#
 model = Ridge(alpha=1000).fit(x_train_extended, y_train)
 weights_hat = model.coef_
 weights_hat[20] = model.intercept_
@@ -37,12 +35,12 @@ weights_hat[20] = model.intercept_
 for i in range(21):
     print(weights_hat[i])
 
+
 # Calculate the Root Mean Squared Error with the known function
 # This only works, because we know the correct prediction function
 #
 # weights_sample =np.arange(1,22)
-#
-# RMSE = mean_squared_error(weights_sample, weights)**0.5
+# RMSE = mean_squared_error(weights_sample, weights_hat)**0.5
 # RMSE_hat = mean_squared_error(weights_sample, weights_hat)**0.5
 # print('---')
 # print(RMSE, RMSE_hat)
