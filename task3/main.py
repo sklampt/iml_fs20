602b6b0383ded78afa402add73e33fd3d11fd060
sklampt <sklampt@student.ethz.ch>
2020-06-15 17:56:00 +0200

    first try task 3: OneHotEncoder, MLPClassifier

--- task3/main.py
+++ task3/main.py
@@ -3,44 +3,32 @@
 ###########################MACHINE LEARNING TASK3###############################
 ################################################################################
 
+import string
 import numpy as np
-from sklearn.metrics import mean_squared_error
-from sklearn.linear_model import Ridge
+import pandas as pd
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import OneHotEncoder
 
-# Load training data
-# np.set_printoptions(precision=16)#set precision for proper data loading
-train = np.loadtxt(
+train = pd.read_csv(
     'data/train.csv',
-    delimiter=',',
-    skiprows=1,
-    dtype=np.float, # change to longdouble doesn't effect anything
 )
 
-y_train = train[:, 1]
-x_train = train[:, 2:]
+test = pd.read_csv(
+    'data/test.csv',
+)
 
-x_train_sq = np.square(x_train)
-x_train_exp = np.exp(x_train)
-x_train_cos = np.cos(x_train)
-ones = np.ones([x_train.shape[0], 1])
+X = train['Sequence'].apply(lambda x: pd.Series(list(x)))
+X_hat = test['Sequence'].apply(lambda x: pd.Series(list(x)))
+print(X)
 
-x_train_extended=np.block([x_train,x_train_sq,x_train_exp,x_train_cos,ones])
-
-
-# Train the model
-model = Ridge(alpha=1000).fit(x_train_extended, y_train)
-weights_hat = model.coef_
-weights_hat[20] = model.intercept_
-
-for i in range(21):
-    print(weights_hat[i])
+enc = OneHotEncoder()
+enc.fit(X)
+onehotlabels = enc.transform(X).toarray()
+train_hotlabels = enc.transform(X_hat).toarray()
 
 
-# Calculate the Root Mean Squared Error with the known function
-# This only works, because we know the correct prediction function
-#
-# weights_sample =np.arange(1,22)
-# RMSE = mean_squared_error(weights_sample, weights_hat)**0.5
-# RMSE_hat = mean_squared_error(weights_sample, weights_hat)**0.5
-# print('---')
-# print(RMSE, RMSE_hat)
+clf = MLPClassifier(random_state=1, max_iter=300).fit(onehotlabels, train['Active'])
+clf.fit(onehotlabels, train['Active'])
+y_pred = clf.predict(train_hotlabels)
+
+print(y_pred)
