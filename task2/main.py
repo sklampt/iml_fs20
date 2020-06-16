d1b8b9f86c540580a4b57fb1fbea94500cc5c048
sklampt <sklampt@student.ethz.ch>
2020-06-16 21:39:45 +0200

    Add some explanations

--- task2/main.py
+++ task2/main.py
@@ -8,38 +8,72 @@ import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn.svm import SVC
 
+# Because each patient has 12 consecutive hours recorded
+# but not the first 12 we have to fix this
+better_time = list(range(12))
+
+# Loading the data from csv
+# Then overwrite the time and set the index
 train_features = pd.read_csv(
     'data/train_features.csv',
-    # index_col=['pid', 'Time'],
 )
+train_features['Time'] = better_time * (len(train_features.index) // 12)
+train_features.set_index(['pid', 'Time'], inplace=True)
 
+# Loading the labels from csv and set the index to pid
 train_labels = pd.read_csv(
     'data/train_labels.csv',
     index_col=['pid'],
 )
 
+# Same for the test data
 test_features = pd.read_csv(
     'data/test_features.csv',
-    index_col=['pid', 'Time'],
 )
+test_features['Time'] = better_time * (len(test_features.index) // 12)
+test_features.set_index(['pid', 'Time'], inplace=True)
 
-train_features['better_time'] = list(range(12)) * 18995
-train_features.set_index(['pid', 'better_time'], inplace=True)
 
-X = train_features[['BaseExcess']].stack(dropna=False).unstack(level=1)
-y = train_labels[['LABEL_BaseExcess']]
+# For subtask 1 we have to predict some labels, we assume that they are
+# only relying on their feature counterpart, ex. BaseExcess -> LABEL_BaseExcess
+subtask1 = [
+    'BaseExcess',
+    'Fibrinogen',
+    'AST',
+    'Alkalinephos',
+    'Bilirubin_total',
+    'Lactate',
+    'TroponinI',
+    'SaO2',
+    'Bilirubin_direct',
+    'EtCO2',
+]
+for feature in subtask1:
+    X = train_features[[feature]].stack(dropna=False).unstack(level=1)
+    X_hat = test_features[[feature]].stack(dropna=False).unstack(level=1)
+    y = train_labels['LABEL_'+feature]
 
-print(X)
-# X = X.transpose()
-imp = SimpleImputer(strategy='mean', copy=False)
-imp.fit_transform(X)
-# X = X.transpose()
-print(X)
-# print(X, y)
-exit(1)
+    imp = SimpleImputer(strategy='mean', copy=False)
+    imp.fit_transform(X)
+    imp.fit_transform(X_hat)
 
-clf = SVC(kernel='sigmoid', class_weight='balanced')
+    # print(X, X_hat, y)
+    # exit(1)
 
-clf.fit(X, y)
+    clf = SVC(kernel='sigmoid', class_weight='balanced')
+    clf.fit(X, y)
 
-print(clf.predict(test_features))
+    y_hat = clf.predict(X_hat)
+
+    # TODO: Save the predictions in the correct way...
+    print(y_hat)
+
+
+# For subtask 2 we have to predict sepsis
+# Symptoms for sepsis include fever, trouble breathing, low blood pressure
+# and high heart rate. Therefore we use each of these as features, not
+# as time series but as mean, variance, min, max and gradient.
+
+
+# Subtask 3: Predict some vitals:
+# 
