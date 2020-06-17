b0d91800088f1caf90fb184d020b4ca82d6d2da1
sklampt <sklampt@student.ethz.ch>
2020-06-17 17:04:38 +0200

    entry =1 only in first row

--- task4/main.py
+++ task4/main.py
@@ -14,54 +14,66 @@ from sklearn.svm import SVC
 train_triplets = pd.read_csv(
     'data/train_triplets.txt',
 )
-print(train_triplets[0,2])
-exit()
+
 # Same for test data
 test_triplets = pd.read_csv(
     'data/test_triplets.txt',
 )
- 
 
-# For subtask 1 we have to predict some labels, we assume that they are
-# only relying on their feature counterpart, ex. BaseExcess -> LABEL_BaseExcess
+# Initialize train and test matrix 
+X = [[0 for x in range(3)] for y in range(59515)]
+X_hat = [[0 for x in range(3)] for y in range(59544)]
+# X[3][2] = 3
+# print(X)
+# exit()
+
+# Create train set with jpg files
+rcounter = 0
 for line in train_triplets:
-    for i in range(3):
-        X[line, i] = Image.open('food/'+train_triplets[line, i]+'.jpg')
+    counter = 0
+    for i in line.split(' '):
+        #print(i)
+        X[rcounter][counter] = 1 #np.array(Image.open('data/food/'+i+'.jpg').getdata())
+        counter += 1
+    rcounter += 1
 print(X)
 exit()
 
 for line in test_triplets:
-    for i in range(3):
-        X_hat[line, i] = Image.open('food/'+test_triplets[line, i]+'.jpg')
+    counter = 0
+    for i in line.split(' '):
+        #print(i)
+        X[line][col] = Image.open('data/food/'+i+'.jpg')
+        counter += 1
+            
+# Split the data into training and test set
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.4, random_state=0
+)
 
-    # Split the data into training and test set
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.4, random_state=0
-    )
+# First Try: Impute and SVC with sigmoid function
+# DOES NOT WORK (maybe its the imputer)
+# TODO: Try with numpy...
+# imp = SimpleImputer(strategy='constant', fill_value=0, copy=False)
+# imp.fit_transform(X_train)
+# imp.fit_transform(X_test)
 
-    # First Try: Impute and SVC with sigmoid function
-    # DOES NOT WORK (maybe its the imputer)
-    # TODO: Try with numpy...
-    # imp = SimpleImputer(strategy='constant', fill_value=0, copy=False)
-    # imp.fit_transform(X_train)
-    # imp.fit_transform(X_test)
+# # print(X, X_hat, y)
+# # exit(1)
 
-    # # print(X, X_hat, y)
-    # # exit(1)
+# clf = SVC(kernel='sigmoid', class_weight='balanced')
+# clf.fit(X_train, y_train)
 
-    # clf = SVC(kernel='sigmoid', class_weight='balanced')
-    # clf.fit(X_train, y_train)
+# y_hat = clf.predict(X_test)
 
-    # y_hat = clf.predict(X_test)
+# Second Try: Tests will be only if there has one yet...
+# DOES NOT WORK
+y_hat = pd.DataFrame(X_test.isnull().sum(axis=1) < 12).astype(int).to_numpy()
 
-    # Second Try: Tests will be only if there has one yet...
-    # DOES NOT WORK
-    y_hat = pd.DataFrame(X_test.isnull().sum(axis=1) < 12).astype(int).to_numpy()
+# TODO: Next Idea would be to use the vitals... could also be useful for sepsis
 
-    # TODO: Next Idea would be to use the vitals... could also be useful for sepsis
-
-    # TODO: Save the predictions in the correct way...
-    print(roc_auc_score(y_test, y_hat))
+# TODO: Save the predictions in the correct way...
+print(roc_auc_score(y_test, y_hat))
 
 
 # For subtask 2 we have to predict sepsis
